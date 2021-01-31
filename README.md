814b12416e0c5b07e46e86081e26aebbe511e842
sabinacolaco <78034115+sabinacolaco@users.noreply.github.com>
2021-02-01 05:00:12 +0800

    API code
    Updated the api code

--- README.md
+++ README.md
@@ -2,5 +2,15 @@
 
 php calculator - using HTML, CSS and PHP
 
+http://localhost/php-calculator/
+
+Database: test
+
+Created API:
+http://localhost/php-calculator/api.php?token=123
+
+Autorization details: apiuser/777
+
+
 
  
--- public/api.php
+++ public/api.php
@@ -1,21 +1,8 @@
 <?php
 header("Content-Type:application/json");
 
-// https://www.php.net/manual/en/features.http-auth.php
-// First check if a username was provided.
-// if (!isset($_SERVER['PHP_AUTH_USER'])) {
-//     // If no username provided, present the auth challenge.
-//     header('WWW-Authenticate: Basic realm="My Website"');
-//     header('HTTP/1.0 401 Unauthorized');
-//     // User will be presented with the username/password prompt
-//     // If they hit cancel, they will see this access denied message.
-//     echo '<p>Access denied. You did not enter a password.</p>';
-//     exit; // Be safe and ensure no other content is returned.
-//}
-
 // If we get here, username was provided. Check password.
 if (isset($_SERVER['PHP_AUTH_USER']) && $_SERVER['PHP_AUTH_USER']=='apiuser' &&  $_SERVER['PHP_AUTH_PW']=='777') {
-    echo '<p>Access granted. You know the password!</p>';
     if (isset($_GET['token']) && $_GET['token']=="123") {
         // Enter your Host, username, password, database below.
         $con = mysqli_connect("localhost","root","","test");
